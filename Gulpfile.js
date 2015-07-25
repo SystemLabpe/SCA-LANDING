a2e10cf70b6d0ab3242ce83e21bd6cae3b44b5e2
Juan Mosquera <juan.mosquera@systemlab.com.pe>
2015-07-25 13:48:39 -0500

    add modules demo

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,10 @@ jshint = require('gulp-jshint'),
 stylish = require('jshint-stylish'),
 inject = require('gulp-inject'),
 wiredep = require('wiredep').stream,
+gulpif = require('gulp-if'),
+minifyCss = require('gulp-minify-css'),
+useref = require('gulp-useref'),
+uglify = require('gulp-uglify'),
 historyApiFallback = require('connect-history-api-fallback');
 
 // Servidor web de desarrollo
@@ -13,10 +17,23 @@ gulp.task('server', function() {
   connect.server({
     root: './app',
     hostname: '0.0.0.0',
-    port: 8080,
+    port: 8081,
     livereload: true,
     middleware: function(connect, opt) {
       return [ historyApiFallback ];
+    }
+  });
+});
+
+// Servidor web para probar el entorno de producción
+gulp.task('server-dist', function() {
+  connect.server({
+    root: './dist',
+    hostname: '0.0.0.0',
+    port: 8080,
+    livereload: true,
+    middleware: function(connect, opt) {
+    return [ historyApiFallback ];
     }
   });
 });
@@ -63,6 +80,30 @@ gulp.task('wiredep', function () {
   .pipe(gulp.dest('./app'));
 });
 
+// Comprime los archivos CSS y JS enlazados en el index.html
+// y los minifica.
+gulp.task('compress', function() {
+  gulp.src('./app/index.html')
+  .pipe(useref.assets())
+  .pipe(gulpif('*.js', uglify({mangle: false })))
+  .pipe(gulpif('*.css', minifyCss()))
+  .pipe(gulp.dest('./dist'));
+});
+
+// Copia el contenido de los estáticos e index.html al directorio
+// de producción sin tags de comentarios
+gulp.task('copy', function() {
+  gulp.src('./app/index.html')
+  .pipe(useref())
+  .pipe(gulp.dest('./dist'));
+  gulp.src('./app/fonts/**')
+  .pipe(gulp.dest('./dist/fonts'));
+  gulp.src('./app/img/**')
+  .pipe(gulp.dest('./dist/img'));
+  gulp.src('./app/video/**')
+  .pipe(gulp.dest('./dist/video'));
+});
+
 // Vigila cambios que se produzcan en el código
 // y lanza las tareas relacionadas
 gulp.task('watch', function() {
@@ -73,3 +114,4 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
+gulp.task('build', ['compress', 'copy']);
--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,24 @@
 "use strict";
-jQuery(document).ready(function($){
+
 var defaultVideoOptions = {
-  controls:true,
+  controls:false,
   autoplay:false,
   preload:'auto',
   width:'auto',
-  height:'auto'
+  height:'auto',
+  muted:true
 };
 
 var videosArray = [];
-var video1 = null, video2 = null;
+var video1 = null, video2 = null, video3 = null,
+    video4 = null, video5 = null, video6 = null;
 
 video1 = videojs('v_prueba_1',defaultVideoOptions, function() {
   this.play();
   this.on('ended', function() {
     video2.play();
   });
-  videosArray.push({element:this,ratio:3/4});
+  videosArray.push({element:this,ratio:9/16});
 });
 
 video2 = videojs('v_prueba_2', defaultVideoOptions, function() {
@@ -24,6 +26,87 @@ video2 = videojs('v_prueba_2', defaultVideoOptions, function() {
     video1.play();
   });
   videosArray.push({element:this,ratio:16/9});
+});
+
+video3 = videojs('v_prueba_3',defaultVideoOptions, function() {
+  this.play();
+  this.on('ended', function() {
+    video4.play();
+  });
+  videosArray.push({element:this,ratio:9/16});
+});
+
+video4 = videojs('v_prueba_4', defaultVideoOptions, function() {
+  this.on('ended', function() {
+    video3.play();
+  });
+  videosArray.push({element:this,ratio:16/9});
+});
+
+video5 = videojs('v_prueba_5',defaultVideoOptions, function() {
+  this.play();
+  this.on('ended', function() {
+    video6.play();
+  });
+  videosArray.push({element:this,ratio:9/16});
+});
+
+video6 = videojs('v_prueba_6', defaultVideoOptions, function() {
+  this.on('ended', function() {
+    video5.play();
+  });
+  videosArray.push({element:this,ratio:16/9});
+});
+
+jQuery(document).ready(function($){
+  var main = $('body');
+  var currentCls = 'home';
+  var modules = {
+    attendance: $('#attendance'),
+    evaluation: $('#evaluation'),
+    task: $('#task')
+  };
+
+  function changeBackground(c){
+    main.removeClass();
+    main.attr('class',c);
+  }
+
+  function isScrolledIntoView(c) {
+    var e = $(window).scrollTop() - 200;
+    var d = e + $(window).height();
+    var a = $(c).offset().top;
+    return ((a <= d) && (a >= e));
+  }
+
+  function isScrolledIntoTopView(c) {
+    var w_scrl_top = $(window).scrollTop();
+    var e_offset   = $(c).offset().top;
+    var e_height   = $(c).height();
+    return (e_offset + e_height >= w_scrl_top);
+  }
+
+  function checkActive(a) {
+    var c = a.data('class');
+    if (c !== currentCls && isScrolledIntoView(a)) {
+      changeBackground(c);
+      currentCls = c;
+    }
+  }
+
+  function onchangeDemoModule(){
+    for (var module in modules) {
+      var a = modules[module];
+      if (modules.hasOwnProperty(module)) {
+        checkActive(a);
+      }
+    }
+  }
+
+  $( window ).scroll(function() {
+    onchangeDemoModule();
+  });
+  onchangeDemoModule();
 });
 
 function resizeVideoJS(){
@@ -35,5 +118,3 @@ function resizeVideoJS(){
 
 resizeVideoJS();
 window.onresize = resizeVideoJS;
-
-});
